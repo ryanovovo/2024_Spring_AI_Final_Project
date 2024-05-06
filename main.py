6147708d14e51c11e20897dd1b53ca5f20ab3938
ryanovovo <ryanovovo@gmail.com>
2024-05-06 20:12:41 +0800

    add unfinetuned test

--- main.py
+++ main.py
@@ -39,6 +39,22 @@ def main(test_times=10):
         gc.collect()
         torch.cuda.empty_cache()
 
+    for prompt_type in prompt_types:
+        df = pd.DataFrame(columns=["Test", "Accuracy", "Precision", "Recall", "F1 Score"])
+        model, tokenizer = load(prompt_type, False)
+        for i in range(test_times):
+            X, y_true = load_validation_data(sample_size=100)
+            y_pred = predict(X, model, tokenizer, prompt_type)
+            accuracy, precision, recall, f1score, conf_matrix = evaluate(y_true, y_pred)
+            print(f"Test {i+1}: Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1 Score: {f1score}")
+            df.loc[len(df)] = {"Test": i+1, "Accuracy": accuracy, "Precision": precision, "Recall": recall, "F1 Score": f1score}
+
+        df.to_csv(f"./results/{prompt_type}_unfinetuned_results.csv")
+        del model
+        del tokenizer
+        gc.collect()
+        torch.cuda.empty_cache()
+
 if __name__ == "__main__":
     main()
 
